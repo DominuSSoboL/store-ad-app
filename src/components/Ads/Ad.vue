09dd3e85b1d6a16d789108ab1414e1466d6d8e85
Igor <sobol.dominus@gmail.com>
2019-02-17 22:23:35 +0200

    Add detal page for card

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,8 +1,22 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12 sm6 offset-sm3>
-                <h1 class="text--secondary md-3">Ad</h1>
+            <v-flex xs12>
+                <v-card>
+                    <v-img
+                        src="https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+                        height="300px"
+                        ></v-img>
+                    <v-card-text>
+                        <h1 class="text--primary">Lorem</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat, sed?</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning">Edit</v-btn>
+                        <v-btn class="success">By</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
