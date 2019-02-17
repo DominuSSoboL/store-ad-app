a0a9efa9a3a6ba1ecd8141069406a278daec9158
Igor <sobol.dominus@gmail.com>
2019-02-17 17:23:20 +0200

    Add slider and start work for cards.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,80 @@
-<template>
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Home</h1>
-            </v-flex>
-        </v-layout>
-    </v-container>
+<template>    
+    <div>
+        <v-container fluid>
+            <v-layout row>
+                <v-flex xs12>
+                    <v-carousel>
+                        <v-carousel-item
+                        v-for="ad in ads"
+                        :key="ad.id"
+                        :src="ad.imageSrc"
+                        ></v-carousel-item>
+                    </v-carousel>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+        <v-container fluid>
+            <v-layout row>
+                <v-flex 
+                    xs4
+                    v-for="ad in ads"
+                    :key="ad.id">
+                    <v-card>
+                        <v-img
+                            :src="ad.imageSrc"
+                            aspect-ratio="2.75"
+                        ></v-img>
+
+                        <v-card-title primary-title>
+                            <div>
+                                <h3 class="headline mb-0">{{ad.title}}</h3>
+                                <div>{{ad.description}}</div>
+                            </div>
+                        </v-card-title>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn flat>Open</v-btn>
+                            <v-btn raised class="primary">By</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </div>
+
+    
 </template>
 
 <script>
 export default {
     data () {
-        return {}
+        return {
+            ads: [
+                {
+                    title: 'First ad',
+                    description: 'Hello i am description',
+                    promo: false,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+                    id: '3'
+                },
+                {
+                    title: 'Second ad',
+                    description: 'Hello i am description',
+                    promo: true,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+                    id: '2'
+                },
+                {
+                    title: 'Third ad',
+                    description: 'Hello i am description',
+                    promo: true,
+                    imageSrc: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+                    id: '3'
+                }
+            ]
+        }
     }
 }
 </script>
