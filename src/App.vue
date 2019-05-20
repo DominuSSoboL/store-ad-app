480fc371bb9f5ee3cae95d9190937b1c1d06b95a
JuliSobol <50823645+JuliSobol@users.noreply.github.com>
2019-05-20 11:08:43 +0300

    Fixed errors

--- src/App.vue
+++ src/App.vue
@@ -60,7 +60,7 @@ export default {
         { title: 'Login', icon: 'lock', url: '/login'},
         { title: 'Registration', icon: 'face', url: '/registration'},
         { title: 'Orders', icon: 'bookmark_border', url: '/orders'},
-        { title: 'New ad', icon: 'note_add', url: '/new'},
+        { title: 'New add', icon: 'note_add', url: '/new'},
         { title: 'My ads', icon: 'list', url: '/list'}
       ]
     }
